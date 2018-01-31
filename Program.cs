cdf6739a61e6b3f7bb1488b964475dbed63f42a0
Koen van der Hoeven <koen@nuc.local>
2018-01-31 23:58:58 +0100

    added one line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace myfirst
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hihihihih");
         }
     }
 }
